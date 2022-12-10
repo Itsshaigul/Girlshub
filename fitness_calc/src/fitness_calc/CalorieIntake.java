fd24a70831043304747bf67b6a91f87ccd30b6bf
IlgizaIsyangildina <ilgizaisyangildina@yandex.ru>
2022-12-10 18:51:43 +0500

    исправление ошибок

--- fitness_calc/src/fitness_calc/CalorieIntake.java
+++ fitness_calc/src/fitness_calc/CalorieIntake.java
@@ -1,5 +1,9 @@
 package fitness_calc;
 
-public class CalorieIntake {
-
+public class CalorieIntake extends Calc{
+	static double nutrientPercentage=1;
+	static double nutrientCalorie=1;
+	CalorieIntake(){
+		super(nutrientPercentage,nutrientCalorie);
+	}
 }
