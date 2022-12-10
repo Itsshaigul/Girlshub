5e733f057497a7b0f9bfa84505beff9e38b62764
IlgizaIsyangildina <ilgizaisyangildina@yandex.ru>
2022-12-10 23:10:13 +0500

    Создание класса для подсчета нормы жиров

--- fitness_calc/src/fitness_calc/Fats.java
+++ fitness_calc/src/fitness_calc/Fats.java
@@ -1,5 +1,9 @@
 package fitness_calc;
 
-public class Fats {
-
+public class Fats extends Calc{
+	static double nutrientPercentage=0.3;
+	static double nutrientCalorie=9;
+	Fats() {
+		super(nutrientPercentage, nutrientCalorie);
+	}
 }
