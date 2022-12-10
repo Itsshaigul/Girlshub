880fdeb059e5d93b31e486ec85831ca63badf2a7
IlgizaIsyangildina <ilgizaisyangildina@yandex.ru>
2022-12-10 23:12:12 +0500

    Создание класса для подсчета нормы белков

---
+++ fitness_calc/src/fitness_calc/Proteins.java
@@ -0,0 +1,9 @@
+package fitness_calc;
+
+public class Proteins extends Calc{
+	static double nutrientPercentage=0.3;
+	static double nutrientCalorie=4;
+	Proteins() {
+		super(nutrientPercentage, nutrientCalorie);
+	}
+}
