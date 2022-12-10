05f7ddaba09781287b181491b6f164ffd993975f
IlgizaIsyangildina <ilgizaisyangildina@yandex.ru>
2022-12-10 22:54:00 +0500

    Создание класса для подсчета нормы углеводов

---
+++ fitness_calc/src/fitness_calc/Carbs.java
@@ -0,0 +1,9 @@
+package fitness_calc;
+
+public class Carbs extends Calc{
+	static double nutrientPercentage=0.4;
+	static double nutrientCalorie=4;
+	Carbs() {
+		super(nutrientPercentage, nutrientCalorie);
+	}
+}
